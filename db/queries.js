82304bc2f1dcf47e4b073ee0d18dd3d7ba41c407
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-14 13:38:39 -0600

    Added New Queries for Read, Post, Update, and Delete

--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,26 @@ const database = require("./database-connection")
 module.exports = {
     list(tableName) {
         return database(tableName).select()
+    },
+    read(id) {
+      return database(tableName).select().where('id', id)
+    },
+    post(newEvent) {
+      return database(tableName)
+        .insert(newEvent)
+        .returning('*')
+        .then(record => record[0])
+    },
+    update(id, newEvent) {
+      return database(tableName)
+        .update(newEvent)
+        .where('id', id)
+        .returning('*')
+        .then(record => record[0])
+    },
+    delete() {
+      return database(tableName)
+        .delete()
+        .where('id', id)
     }
 }
